00846836e0da398f42151257017b6c32698b14ea
JorgeML01 <jorgedd2001@gmail.com>
2024-01-07 01:06:03 -0500

    background video content added

--- src/components/Dashboard/DashboardVideo/DashboardVideo.jsx
+++ src/components/Dashboard/DashboardVideo/DashboardVideo.jsx
@@ -1,15 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import Container from "react-bootstrap/Container";
 
 function DashboardVideo() {
   return (
-    <video className="VideoTag" autoPlay loop muted>
-      <source
-        src="https://video.wixstatic.com/video/11062b_989ef4fbeff04227933fa60f858282fe/1080p/mp4/file.mp4"
-        type="video/mp4"
-      />
-    </video>
+    <div className="video-background">
+      <video className="VideoTag" autoPlay loop muted>
+        <source
+          src="https://video.wixstatic.com/video/11062b_989ef4fbeff04227933fa60f858282fe/1080p/mp4/file.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div>
+        <div className="video-background-content div1">ONE CALL,</div>
+        <div className="video-background-content div2">CLEANS IT ALL</div>
+        <div className="video-background-content div3">
+          Experience our Fresh & Express Cleanliness,
+        </div>
+        <div className="video-background-content div4">
+          Faster than You Can Blink
+        </div>
+      </div>
+      <div id="button-call-now">Call Now</div>
+    </div>
   );
 }
 
