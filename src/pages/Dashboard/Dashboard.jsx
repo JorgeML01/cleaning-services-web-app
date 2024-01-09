48a6ecf55014042d77ea5313d3caac99a3922d48
JorgeML01 <jorgedd2001@gmail.com>
2024-01-09 17:24:09 -0500

    fixed bugs

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,6 @@ import DashboardVideo from "../../components/Dashboard/DashboardVideo";
 import MainTitle from "../../components/Dashboard/Services/MainTitle/MainTitle.jsx";
 import ServiceCards from "../../components/Dashboard/Services/ServiceCards/ServiceCards.jsx";
 import "./styles.css";
-import { Container, Row, Col, Card } from "react-bootstrap";
 
 function Dashboard() {
   return (
