f32cfe402d9bf74b63ad4ff6bf8781224dac68c4
JorgeML01 <jorgedd2001@gmail.com>
2024-01-15 12:14:23 -0600

    about us padding

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -5,23 +5,22 @@ function AboutUs() {
     <div className="about-us-container">
       <div className="about-us-title">About Us</div>
       <div className="about-us-content">
-        Remember, integrating third-party libraries like Flowbite into a React
-        application might require additional considerations to ensure proper
-        integration with the React component lifecycle. Always refer to the
-        documentation of the library for any specific React-related
-        instructions. Remember, integrating third-party libraries like Flowbite
-        into a React application might require additional considerations to
-        ensure proper integration with the React component lifecycle. Always
-        refer to the documentation of the library for any specific React-related
-        instructions. Remember, integrating third-party libraries like Flowbite
-        into a React application might require additional considerations to
-        ensure proper integration with the React component lifecycle. Always
-        refer to the documentation of the library for any specific React-related
-        instructions. Remember, integrating third-party libraries like Flowbite
-        into a React application might require additional considerations to
-        ensure proper integration with the React component lifecycle. Always
-        refer to the documentation of the library for any specific React-related
-        instructions.
+        Welcome to TidyTime cleaning services, where spotless spaces and your
+        satisfaction are our top priorities. With a focus on excellence, we
+        offer a wide array of premium cleaning services tailored to meet your
+        needs. From meticulous residential and commercial cleaning to
+        post-construction and moving in/out solutions, we've mastered the art of
+        pristine spaces. In a fast-paced world, maintaining cleanliness can be a
+        challenge. That's why we're here – to provide professional, reliable,
+        and top-tier cleaning services that understand the value of your time
+        and the importance of a hygienic environment. At TidyTime cleaning
+        services, every client is cherished. We're committed to delivering
+        beyond expectations, ensuring your complete satisfaction with every
+        service. Equipped with cutting-edge tools and a skilled team, we
+        guarantee the highest standards of cleanliness for your space. Whatever
+        your cleaning needs might be, rest assured, we've got you covered.
+        Experience the difference with TidyTime cleaning services – where
+        exceptional quality meets unmatched dedication to our customers.
       </div>
     </div>
   );
